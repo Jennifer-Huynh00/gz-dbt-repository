01676a972face8c1d2455d35bb5d9a9ccfb1cf85
Jennifer Huynh <jennifer.huynh00@gmail.com>
2025-11-04 11:32:39 +0000

    Changes column and data type 2

--- models/staging/gz_raw_data/stg_gz_raw_data__product.sql
+++ models/staging/gz_raw_data/stg_gz_raw_data__product.sql
@@ -10,10 +10,9 @@ renamed as (
 
     select
         products_id,
-        purchse_price AS purchase_price
-        CAST (purchase_price AS FLOAT64 )
-
+    CAST (purchse_price AS FLOAT64 ) AS purchase_price
     from source
+
 
 )
 
