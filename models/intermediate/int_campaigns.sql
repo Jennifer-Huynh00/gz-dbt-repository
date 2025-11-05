366d1132d0cc9afaee92aa4bafa4aace81cb0829
Jennifer Huynh <jennifer.huynh00@gmail.com>
2025-11-05 15:09:52 +0000

    remove .sql int_campaign

--- models/intermediate/int_campaigns.sql
+++ models/intermediate/int_campaigns.sql
@@ -1,10 +1,11 @@
 SELECT *
- FROM {{ref('stg_gz_raw_data__adwords.sql')}}
+ FROM {{ref('stg_gz_raw_data__adwords')}}
  UNION ALL
  SELECT *
- FROM {{ref('stg_gz_raw_data__bing.sql')}}
- SELECT *
- FROM {{ref('stg_gz_raw_data__criteo.sql')}}
+ FROM {{ref('stg_gz_raw_data__bing')}}
  UNION ALL
  SELECT *
- FROM {{ref('stg_gz_raw_data__facebook.sql')}}
+ FROM {{ref('stg_gz_raw_data__criteo')}}
+ UNION ALL
+ SELECT *
+ FROM {{ref('stg_gz_raw_data__facebook')}}
