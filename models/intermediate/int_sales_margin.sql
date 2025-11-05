7b0c640764103e383f1834f14ee3e6d098903b52
Jennifer Huynh <jennifer.huynh00@gmail.com>
2025-11-05 14:14:50 +0000

    Using Macros in Your Models

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,3 +1,5 @@
+WITH sales_purchase_cost AS(
+
 select
 *,
 quantity*purchase_price AS purchase_cost,
@@ -5,3 +7,9 @@ ROUND(revenue - (quantity*purchase_price),2) AS margin
 from {{ ref('stg_gz_raw_data__sales')}}
 join {{ref('stg_gz_raw_data__product')}}
 USING(products_id)
+)
+
+select
+*,
+{{margin_percent('revenue','purchase_cost')}} AS margin_percent,
+FROM sales_purchase_cost
